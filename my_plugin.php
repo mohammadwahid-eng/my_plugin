dbe3bed0f13d4f586a1084af5b2591d3a2ebbfdd
Mohammad Wahid <5797029+mohammadwahid-eng@users.noreply.github.com>
2023-04-05 22:30:21 +0600

    Bug fix

--- my_plugin.php
+++ my_plugin.php
@@ -4,13 +4,13 @@ Plugin Name: My Plugin
 Plugin URI: http://mohammadwahid.site/myplugin/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Mohammad Wahid
-Version: 1.0.0
+Version: 1.0.1
 Author URI: http://mohammadwahid.site/
 */
 
 if( ! function_exists('my_plugin') ) {
 	function my_plugin() {
-		echo "hello world";
+		echo "hello plugin";
 	}
 	add_action( 'wp_head', 'my_plugin' );
 }
